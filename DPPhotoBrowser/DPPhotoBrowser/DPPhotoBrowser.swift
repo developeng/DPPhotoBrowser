e0947155257618aab2323aa43e48973468ceaa02
developeng <developeng@163.com>
2021-02-23 15:01:31 +0800

    1

--- DPPhotoBrowser/DPPhotoBrowser/DPPhotoBrowser.swift
+++ DPPhotoBrowser/DPPhotoBrowser/DPPhotoBrowser.swift
@@ -7,6 +7,18 @@
 
 import UIKit
 
+let DP_SCREEN_WIDTH = UIScreen.main.bounds.size.width
+let DP_SCREEN_HEIGHT = UIScreen.main.bounds.size.height
+
+let DP_STATUSBAR_HEIGHT = UIApplication.shared.statusBarFrame.size.height
+
+let DP_IS_IPHONE_X = DP_STATUSBAR_HEIGHT > 20 ? true : false
+
+let DP_STATUS_AND_NAV_BAR_HEIGHT:CGFloat = DP_IS_IPHONE_X == true ? 88.0 : 64.0
+let DP_NAVBAR_HEIGHT:CGFloat = DP_IS_IPHONE_X == true ? 44.0 : 20.0
+let DP_TABBAR_HEIGHT:CGFloat = DP_IS_IPHONE_X == true ? 83.0 : 49.0
+let DP_BOTTOM_SAFE_HEIGHT:CGFloat = DP_IS_IPHONE_X == true ? 34 : 0
+
 class DPPhotoBrowser: NSObject {
     /// 查看大图
     /// - Parameters:
